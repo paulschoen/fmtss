ac7f5eb1a876680c6348f446470a0f7c228878b6
OpenShift guest <schoen14580p@gmail.com>
2013-04-16 21:41:13 -0400

    Initial Rails Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fmtss::Application.config.secret_token = '282d3b338f80c79597af4ecf3d799c66717b1f5d63dfa2795ed52606cc4306d01fee26242fb4f33b0ec000e9ed1987bf6790c5ad466e97e690973af9b01f0809'
