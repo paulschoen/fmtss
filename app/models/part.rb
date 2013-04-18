764ef62568877163dfd57bc0c36dbd3003dc0303
Bill Barnett <bill@gaslight.co>
2013-04-18 01:11:53 -0400

    Remove spurious user code

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,7 +1,3 @@
 class Part < ActiveRecord::Base
   attr_accessible :description, :item, :oem_part_number, :part_number, :quantity
 end
-class User < ActiveRecord::Base
-  attr_accessible :avatar
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-end
--- db/migrate/20130417014644_create_parts.rb
+++ db/migrate/20130417014644_create_parts.rb
@@ -11,12 +11,3 @@ class CreateParts < ActiveRecord::Migration
     end
   end
 end
-class AddAvatarColumnsToUsers < ActiveRecord::Migration
-  def self.up
-    add_attachment :users, :avatar
-  end
-
-  def self.down
-    remove_attachment :users, :avatar
-  end
-end
